7da058204fea0ac24eb6e09f903d4de70e642838
YaoYinYing <33014714+YaoYinYing@users.noreply.github.com>
2023-10-20 18:07:17 +0800

    Update REvoDesign.PyMOL.py

--- REvoDesign.PyMOL.py
+++ REvoDesign.PyMOL.py
@@ -6,7 +6,7 @@ logging.info(f'REvoDesign UI is installed in {os.path.dirname(__file__)}')
 
 sys.path.append(os.path.dirname(__file__))
 
-from revodesign import REvoDesignPlugin
+from REvoDesign import REvoDesignPlugin
 
 # entrypoint of PyMOL plugin
 def __init_plugin__(app=None):
