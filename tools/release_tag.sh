dd241c1576c33ea0b6ef85b401fe78435c7de205
YaoYinYing <33014714+YaoYinYing@users.noreply.github.com>
2024-01-09 16:14:43 +0800

    update release_tag.sh

--- tools/release_tag.sh
+++ tools/release_tag.sh
@@ -1,31 +1,36 @@
 #! bash
+echo 'Dumping version from `REvoDesign/__version__.py` ...'
 old_version=$(git diff REvoDesign/__version__.py | grep '^\-__version__=' | awk '{str=$2;gsub("'\''","",str);print str}')
 new_version=$(git diff REvoDesign/__version__.py | grep '^+__version__=' | awk '{str=$2;gsub("'\''","",str);print str}')
 new_date=$(date +'%Y-%m-%d')
 
+echo "New Version: ${new_version}, Old Version: ${old_version}, tagged date: ${new_date}"
+
+echo 'Checking version ...'
 # exit if any of the variables are missing.
 if [[ ! $new_version || ! $old_version || ! $new_date  ]];then 
-    echo "Error: $new_version , $old_version, $$new_date"
+    echo "Error: $new_version , $old_version, $new_date"
     exit 1;
 fi
 if [[ $new_version == $old_version ]];then 
-    echo same version number: $new_version == $old_version;
+    echo Same version number: $new_version == $old_version;
     exit 1;
 fi
+echo 'Done.'
 
-# set new tag to changelog
+echo set new tag to changelog
 sed -i 's/## \[Unreleased\]/## [Unreleased]\n\n## \['"$new_version"'\] - '"$new_date"'/' ./CHANGELOG.md
 
-# set new tag to changelog
+echo set new tag to pyproject
 sed -i 's/^version = \"'"$old_version"'\"/version = \"'"$new_version"'\"/' ./pyproject.toml
 
-# collect version files
+echo  collect version files and creating new commit...
 git add ./CHANGELOG.md ./pyproject.toml REvoDesign/__version__.py
 # version commit
 git commit -m 'Dump version: '"$old_version"' -> '"$new_version"''
-# push
+echo pushing new version
 git push 
 
-# set git tag
+echo set git tag ..
 git tag v$new_version
 git push origin --tags
