3e47f12c70f9357b1babaf6e1601ceff7e10bb33
YaoYinYing <33014714+YaoYinYing@users.noreply.github.com>
2024-04-16 17:31:16 +0800

    Update __init__.py

--- REvoDesign/phylogenetics/__init__.py
+++ REvoDesign/phylogenetics/__init__.py
@@ -1168,10 +1168,10 @@ class GREMLIN_Analyser:
                 worker_function=self.gremlin_external_scorer.scorer,
                 sequence=mutant_obj.get_mutant_sequence_single_chain(
                     chain_id=self.design_chain_id,
-                    ignore_missing=True,
+                    ignore_missing=True),
                     progress_bar=self.bus.ui.progressBar,
-                ),
-            )
+                )
+
 
         mutant_obj.wt_score = wt_score
         mutant_obj.mutant_score = mut_score
