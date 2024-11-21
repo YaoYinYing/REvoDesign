7ef41a35fda963c02798a58f197e7a6e319f6f25
YaoYinYing <33014714+YaoYinYing@users.noreply.github.com>
2024-11-21 17:43:34 +0800

    chore:parametric-register

--- src/REvoDesign/driver/param_toggle_register.py
+++ src/REvoDesign/driver/param_toggle_register.py
@@ -1,5 +1,9 @@
+from typing import Protocol, Tuple
+
 from ..tools.customized_widgets import refresh_widget_while_another_changed
 from ..sidechain_solver.SidechainSolver import all_runner_c
+from ..common.ProfileParsers import all_parser_classes
+from ..external_designer import all_designer_classes
 from ..basic import ParamChangeRegistryItem as PCRI, ParamChangeRegister as PCR
 
 
@@ -17,11 +21,43 @@ ParamChangeSidechainSolverWeights=PCRI(
 )
 
 
+class ParserOrDesigner(Protocol):
+    name: str
+    prefer_lower: bool
+
+all_profile_or_designers: Tuple[type[ParserOrDesigner]]=all_parser_classes + tuple(all_designer_classes) # type: ignore
+
+
+profile_or_designer_vs_is_prefer_lower_score={profile_or_designer.name: (profile_or_designer.prefer_lower,) for profile_or_designer in all_profile_or_designers}
+
+ParamChangePreferLowerScoreTabMutate=PCRI(
+    "comboBox_profile_type",
+    "currentIndexChanged",
+    "ui.mutate.input.profile_type",
+    "ui.header_panel.cmap.reverse_score",
+    profile_or_designer_vs_is_prefer_lower_score
+
+)
+
+ParamChangePreferLowerScoreTabVisualize=PCRI(
+    "comboBox_profile_type_2",
+    "currentIndexChanged",
+    "ui.visualize.input.profile_type",
+    "ui.header_panel.cmap.reverse_score",
+    profile_or_designer_vs_is_prefer_lower_score
+
+)
+
+
 # collect all of these cases
 
 ParamChangeCollections=PCR(
     register_func=refresh_widget_while_another_changed,
-    registry=(ParamChangeSidechainSolverWeights,)
+    registry=(
+        ParamChangeSidechainSolverWeights,
+        ParamChangePreferLowerScoreTabMutate,
+        ParamChangePreferLowerScoreTabVisualize
+        )
     )
 
 
