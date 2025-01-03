3db083280a34c762615c63231451207e112971bf
YaoYinYing <33014714+YaoYinYing@users.noreply.github.com>
2025-01-03 13:42:10 +0800

    Bump version: 1.7.12 -> 1.7.13
    [ci skip]

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -18,6 +18,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [1.7.13] - 2025-01-03
+
 ### Added
 
 - Utils: 
--- src/REvoDesign/__init__.py
+++ src/REvoDesign/__init__.py
@@ -25,7 +25,7 @@ from REvoDesign.REvoDesign import REvoDesignPlugin
 from REvoDesign.shortcuts import __all__ as all_shortcuts
 
 # 7. Set version info
-__version__ = "1.7.12"
+__version__ = "1.7.13"
 
 # 8. enable garbage collection
 gc.enable()
