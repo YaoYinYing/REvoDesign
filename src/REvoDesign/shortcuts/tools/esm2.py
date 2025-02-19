b2f50a9c5be49022bca9b3398b8de924069bd286
YaoYinYing <33014714+YaoYinYing@users.noreply.github.com>
2025-02-20 01:54:40 +0800

    Update esm2.py

--- src/REvoDesign/shortcuts/tools/esm2.py
+++ src/REvoDesign/shortcuts/tools/esm2.py
@@ -131,14 +131,18 @@ class Esm1v:
 
         alphabet = "ARNDCQEGHILKMFPSTWYV"
         if self.skip_wt:
-            df_dms = pd.DataFrame([f'{self.sequence[idx]}{idx+1}{mut}' for (idx,
-                                                                            mut) in itertools.product(range(0,
-                                                                                                            len(self.sequence)),
-                                                                                                      alphabet) if self.sequence[idx] != mut],
-                                  columns=[self.mutation_col])
+            df_dms = pd.DataFrame(
+                [
+                    f'{self.sequence[idx]}{idx+1}{mut}' 
+                    for idx,mut in itertools.product(range(0,len(self.sequence)),alphabet) 
+                    if self.sequence[idx] != mut
+                ], columns=[self.mutation_col])
         else:
-            df_dms = pd.DataFrame([f'{self.sequence[idx]}{idx+1}{mut}' for (idx, mut)
-                                  in itertools.product(range(0, len(self.sequence)), alphabet)], columns=[self.mutation_col])
+            df_dms = pd.DataFrame(
+                [
+                    f'{self.sequence[idx]}{idx+1}{mut}' 
+                    for idx, mut in itertools.product(range(0, len(self.sequence)), alphabet)
+                ], columns=[self.mutation_col])
         return df_dms
 
     def predict(self):
