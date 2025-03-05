4c6f45bd42d57d1639c2610c95df408cd65483cd
YaoYinYing <33014714+YaoYinYing@users.noreply.github.com>
2025-03-05 21:05:07 +0800

    test

--- src/REvoDesign/tools/cgo_utils.py
+++ src/REvoDesign/tools/cgo_utils.py
@@ -2423,15 +2423,15 @@ class GraphicObjectCollection(GraphicObject):
 # with timing('writing'):
 #     test_text()
 
-# font_path = "/Library/Fonts/Microsoft/Microsoft Yahei.ttf"
-# text='APTX-4869\n\nsilver bullet\n\nCool Kid'
+font_path = "/Users/yyy/Downloads/simhei.ttf"
+text='Silver Bullet\n\nCool Kid'
 
 
-# TextBoard(
-#     text=text,
-#     font_path=font_path,
-#     width=10
-# ).load_as('silver_bullet')
+TextBoard(
+    text=text,
+    font_path=font_path,
+    width=1.5, space=10
+).load_as('silver_bullet')
 
 
 # also a quick demo to construct complicated cgo object
