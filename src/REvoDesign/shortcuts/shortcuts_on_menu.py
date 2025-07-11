6603e3de0760213e25e5dec8fa577b38c2538a9c
Yinying Yao <33014714+YaoYinYing@users.noreply.github.com>
2025-07-11 16:38:58 +0800

    save

--- src/REvoDesign/shortcuts/shortcuts_on_menu.py
+++ src/REvoDesign/shortcuts/shortcuts_on_menu.py
@@ -3,16 +3,17 @@ This module contains the menu shortcuts for REvoDesign.
 '''
 # To create a dialog form, one must implement a wrapper function that
 # decorated by `dialog_wrapper` and import it here
-from REvoDesign.shortcuts.wrappers.designs import (wrapped_profile_pick_design,
-                                                   wrapped_pssm2csv)
+
+from REvoDesign.shortcuts.wrappers.designs import (
+    wrapped_profile_pick_design, wrapped_pssm2csv)
 from REvoDesign.shortcuts.wrappers.esm2 import wrapped_esm1v
 from REvoDesign.shortcuts.wrappers.exports import (
     wrapped_dump_fasta_from_struct, wrapped_menu_dump_sidechains)
 from REvoDesign.shortcuts.wrappers.ligand_converters import (
     wrapped_smiles_conformer_batch, wrapped_smiles_conformer_single,
     wrapper_sdf2rosetta_params)
-from REvoDesign.shortcuts.wrappers.mutation_effect_predictors import \
-    wrapped_thermompnn
+from REvoDesign.shortcuts.wrappers.mutation_effect_predictors import (
+    wrapped_thermompnn)
 from REvoDesign.shortcuts.wrappers.represents import (
     wrapped_color_by_mutation, wrapped_color_by_plddt, wrapped_real_sc)
 from REvoDesign.shortcuts.wrappers.rfdiffusion_tasks import (
@@ -20,23 +21,17 @@ from REvoDesign.shortcuts.wrappers.rfdiffusion_tasks import (
 from REvoDesign.shortcuts.wrappers.rosetta_tasks import (
     wrapped_fast_relax, wrapped_pross, wrapped_relax_w_ca_constraints,
     wrapped_rosettaligand)
-from REvoDesign.shortcuts.wrappers.structure import wrapped_resi_renumber
-from REvoDesign.shortcuts.wrappers.utils import wrapped_logger_level_setter
-from REvoDesign.shortcuts.wrappers.vina_tools import (wrapped_alterbox,
-                                                      wrapped_get_pca_box,
-                                                      wrapped_getbox,
-                                                      wrapped_rmhet)
+from REvoDesign.shortcuts.wrappers.structure import (
+    wrapped_resi_renumber)
+from REvoDesign.shortcuts.wrappers.utils import (
+    wrapped_logger_level_setter)
+from REvoDesign.shortcuts.wrappers.vina_tools import (
+    wrapped_alter_box, wrapped_get_pca_box, wrapped_getbox, wrapped_rmhet)
 from REvoDesign.tools.customized_widgets import AskedValue
 from REvoDesign.tools.pymol_utils import get_all_groups
 
 
 def menu_dump_sidechains(dump_all=False):
-    """
-    Prepares and launches the sidechain dumping menu.
-
-    Args:
-        dump_all (bool): If True, preselects all groups for sidechain dumping.
-    """
     dynamic_value = {
         "value": AskedValue(
             "sele",
@@ -46,168 +41,32 @@ def menu_dump_sidechains(dump_all=False):
             choices=get_all_groups(),
             multiple_choices=True
         ),
-        "index": 0,  # Specify the position in the options list
+        "index": 0,
     }
-
     wrapped_menu_dump_sidechains([dynamic_value])
 
 
-def menu_color_by_plddt():
-    """
-    Launches the wrapped dialog for coloring by pLDDT values.
-
-    Dynamic values, if any, can be appended here before invoking the wrapped function.
-    """
-    wrapped_color_by_plddt()
-
-
-def menu_pssm2csv():
-    """
-    Launches the dialog for PSSM to CSV conversion.
-    """
-    wrapped_pssm2csv()
-
-
-def menu_real_sc():
-    """
-    Launches the dialog for setting sidechain representation.
-    """
-    wrapped_real_sc()
-
-
-def menu_color_by_mutation():
-    '''
-    Launches the dialog for coloring by mutation.
-    '''
-
-    wrapped_color_by_mutation()
-
-
-def menu_smiles_conformer_single():
-    """
-    Launches the dialog for generating 3D conformers for a SMILES string.
-    """
-    wrapped_smiles_conformer_single()
-
-
-def menu_smiles_conformer_batch():
-    """
-    Launches the dialog for generating 3D conformers for multiple SMILES strings.
-    """
-    wrapped_smiles_conformer_batch()
-
-
-def menu_profile_pick_design():
-    """
-    Launches the dialog for profile pick design.
-    """
-    return wrapped_profile_pick_design()
-
-
-def menu_resi_renumber():
-    """
-    Launches the dialog for residue renumbering.
-    """
-    wrapped_resi_renumber()
-
-
-def menu_dump_fasta_from_struct():
-    """
-    Launches the dialog for dumping sequences from a structure.
-    """
-    wrapped_dump_fasta_from_struct()
-
-
-def menu_sdf2rosetta_params():
-    """
-    Launches the dialog for converting SDF to Rosetta parameters.
-    """
-    wrapper_sdf2rosetta_params()
-
-
-def menu_rosettaligand():
-    """
-    Launches the dialog for docking a ligand to a protein.
-    """
-    wrapped_rosettaligand()
-
-
-def menu_pross():
-    """
-    Launches the dialog for PROSS design dialog
-    """
-    wrapped_pross()
-
-
-def menu_fast_relax():
-    """
-    Launches the dialog for fast relax dialog
-    """
-    wrapped_fast_relax()
-
-
-def menu_relax_w_ca_constraints():
-    """
-    Launches the dialog for relax with CA constraints dialog
-    """
-    wrapped_relax_w_ca_constraints()
-
-
-def menu_thermompnn():
-    """
-    Launches the dialog for ThermoMPNN dialog
-    """
-    wrapped_thermompnn()
-
-
-def menu_esm1v():
-    """
-    Launches the dialog for ESM1v dialog
-    """
-    wrapped_esm1v()
-
-
-def menu_alterbox():
-    """
-    Launches the dialog for ESM1v dialog
-    """
-    wrapped_alterbox()
-
-
-def menu_get_pca_box():
-    """
-    Launches the dialog for ESM1v dialog
-    """
-    wrapped_get_pca_box()
-
-
-def menu_getbox():
-    """
-    Launches the dialog for ESM1v dialog
-    """
-    wrapped_getbox()
-
-
-def menu_rmhet():
-    """
-    Launches the dialog for ESM1v dialog
-    """
-    wrapped_rmhet()
-
-
-def menu_general_rfdiffusion_task():
-    """
-    Launches the dialog for general_rfdiffusion_task dialog
-    """
-    wrapped_general_rfdiffusion_task()
-
-
-def menu_visualize_substrate_potentials():
-    """
-    Launches the dialog for visualize_substrate_potentials dialog
-    """
-    wrapped_visualize_substrate_potentials()
-
-
-def menu_logger_level_setter():
-    wrapped_logger_level_setter()
+# One-liner bindings
+menu_color_by_plddt = wrapped_color_by_plddt
+menu_pssm2csv = wrapped_pssm2csv
+menu_real_sc = wrapped_real_sc
+menu_color_by_mutation = wrapped_color_by_mutation
+menu_smiles_conformer_single = wrapped_smiles_conformer_single
+menu_smiles_conformer_batch = wrapped_smiles_conformer_batch
+menu_profile_pick_design = wrapped_profile_pick_design
+menu_resi_renumber = wrapped_resi_renumber
+menu_dump_fasta_from_struct = wrapped_dump_fasta_from_struct
+menu_sdf2rosetta_params = wrapper_sdf2rosetta_params
+menu_rosettaligand = wrapped_rosettaligand
+menu_pross = wrapped_pross
+menu_fast_relax = wrapped_fast_relax
+menu_relax_w_ca_constraints = wrapped_relax_w_ca_constraints
+menu_thermompnn = wrapped_thermompnn
+menu_esm1v = wrapped_esm1v
+menu_alterbox = wrapped_alter_box
+menu_get_pca_box = wrapped_get_pca_box
+menu_getbox = wrapped_getbox
+menu_rmhet = wrapped_rmhet
+menu_general_rfdiffusion_task = wrapped_general_rfdiffusion_task
+menu_visualize_substrate_potentials = wrapped_visualize_substrate_potentials
+menu_logger_level_setter = wrapped_logger_level_setter
