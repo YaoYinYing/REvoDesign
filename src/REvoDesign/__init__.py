a1cd8c016cff3f3780fb434582db7869c0ad2ad7
YaoYinYing <33014714+YaoYinYing@users.noreply.github.com>
2025-09-28 13:10:46 +0800

    Update __init__.py

--- src/REvoDesign/__init__.py
+++ src/REvoDesign/__init__.py
@@ -25,7 +25,7 @@ from REvoDesign.REvoDesign import REvoDesignPlugin
 from REvoDesign.shortcuts import __all__ as all_shortcuts
 
 # 8. Set version info
-__version__ = "1.8.3"
+__version__ = "1.8.3-iGEM2025"
 
 # 9. enable garbage collection
 gc.enable()
