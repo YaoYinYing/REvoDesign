903e475a6f08f9794ba3091b71488d1019229289
YaoYinYing <33014714+YaoYinYing@users.noreply.github.com>
2025-11-13 15:23:41 +0800

    save

--- tests/citations/test_get_cited.py
+++ tests/citations/test_get_cited.py
@@ -6,13 +6,13 @@ from REvoDesign.tools.utils import get_cited
 
 class MyModule(CitableModuleAbstract):
     @get_cited
-    def test_instance_method(self):
-        print("Test instance method called")
+    def test_instance_method(self, key: str = 'awesome!'):
+        print(f"Test instance method called: {key}")
 
     @classmethod
     @get_cited
-    def test_classmethod(cls):
-        print("Test classmethod called")
+    def test_classmethod(cls, key: str = 'awesome!'):
+        print(f"Test classmethod called: {key}")
 
     @staticmethod
     @get_cited
@@ -24,22 +24,27 @@ def my_function():
     print("My function called")
 
 
-@pytest.mark.parametrize("citation_header, method_name", [
+@pytest.mark.parametrize("citation_header, method_name, kwargs", [
     # class level methods
-    # ["modulex_method", 'test_instance_method'], # not possible to test instance method at class level
-    ["modulex_class_method", 'test_classmethod'],
-    ["modulex_static_method", 'test_staticmethod'],
+    # not possible to test instance method at class level
+    ["modulex_class_normal_method", 'test_classmethod', None],
+    ["modulex_static_method", 'test_staticmethod', None],
+    ["modulex_class_method_kwargs", 'test_classmethod', {'key': 'great!'}],
+    ["modulex_static_method_kwargs", 'test_staticmethod', {'key': 'great!'}],
     # object level methods
-    ["modulex_object_method", 'test_instance_method'],
-    ["modulex_object_class_method", 'test_classmethod'],
-    ["modulex_object_static_method", 'test_staticmethod']
+    ["modulex_object_normal_method", 'test_instance_method', None],
+    ["modulex_object_class_method", 'test_classmethod', None],
+    ["modulex_object_static_method", 'test_staticmethod', None],
+    ["modulex_object_method_kwargs", 'test_instance_method', {'key': 'great!'}],
+    ["modulex_object_class_method_kwargs", 'test_classmethod', {'key': 'great!'}],
+    ["modulex_object_static_method_kwargs", 'test_staticmethod', {'key': 'great!'}],
 ])
-def test_get_cited_class(citation_header, method_name):
+def test_get_cited_class(citation_header, method_name, kwargs):
     MyModule.__bibtex__ = {citation_header: "citationx"}
     module = MyModule() if 'object' in citation_header else MyModule
     assert citation_header not in CitationManager().called_citations
     method = getattr(module, method_name)
-    method()
+    method(**kwargs or {})
     assert citation_header in CitationManager().called_citations
 
 
